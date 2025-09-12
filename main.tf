83fde3f8e6194f4be46e288486dd0d4a3281db99
Jon Bevan <jbevan@adaptavist.com>
2025-09-12 15:08:10 +0100

    fix: use random chars for cloudformation stack
    BREAKING CHANGE: Stop using SSM param name in name of cloudformation stack.
    
    This allows changes to the SSM param name to be propagated to the underlying lambda function that backs the secret generation, as opposed to changing the cloudformation stack if the SSM param name changes

--- main.tf
+++ main.tf
@@ -20,8 +20,13 @@ locals {
   finalTags = merge(var.tags, local.stageTag)
 }
 
+# this is generated at initial resource creation and then not modified afterwards
+resource "random_id" "cf_stack" {
+  byte_length = 8
+}
+
 resource "aws_cloudformation_stack" "execute_lambda" {
-  name = "create-secret${replace(var.secret_ssm_path, "/[:_/.]/", "-")}-execution-stack${var.name_suffix}"
+  name = "create-secret-${random_id.cf_stack.id}-execution-stack${var.name_suffix}"
 
   timeout_in_minutes = "3"
   template_body = <<EOF
